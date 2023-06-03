8bad64342c0592ba4907c701eab2b2a00e76a538
JohnsonMasino <johnsonmasino@gmail.com>
2023-06-03 14:42:21 -0700

    Practicals

---
+++ Fruit_House_Project/more/caller.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""
+This method calls other functions
+"""
+import defining
+
+def Him():
+    print(defining.Second)
+
---
+++ Fruit_House_Project/more/defining.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+This file defines a function and gives it a role
+"""
+
+def Printing():
+    """Prints some statements"""
+    print("Enter your name here")
+    print("Thank you foir tuning in")
+
+def Second():
+    """prints some like statements"""
+    print("Choose a or b now")
+    print("More")
+
+if __name__ == "__main__":
+    Printing()
+    Second()
+
+print("\nCode developed by Masino")
