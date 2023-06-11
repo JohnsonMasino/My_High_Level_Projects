7c72e6d5a3d753dc7f4e7d24ea4d7e6ff4e55a8c
JohnsonMasino <johnsonmasino@gmail.com>
2023-06-10 17:11:00 -0700

    Changed

--- The_Calculator_Project/numi_main_file.py
+++ The_Calculator_Project/numi_main_file.py
@@ -13,34 +13,35 @@ class NumiCalculator():
         """
         expression = input("Please enter your mathematical expression here. Division in word means 'over': ")
         done = expression.split(" ")
+
         if len(done) == 3:
             """indicates only two values calculation
             """
+            """if not isinstance(done[0], int):
+                raise TypeError('First input must be a number please...')
+            if not isinstance(done[2], int):
+                raise TypeError('Second input must be a number please...')"""
+
+            index0 = int(done[0])
+            index1 = int(done[2])
             if done[1] == '+' or done[1] == 'Plus' or done[1] == 'plus' or done[1] == 'PLUS':
                 """calls the addition function only
                 """
-                index0 = int(done[0])
-                index1 = int(done[2])
                 print(NumiCalculator.a0(index0, index1))
             elif done[1] == '-' or done[1] == 'Minus' or done[1] == 'minus' or done[1] == 'MINUS':
                 """calls the subtraction function only
                 """
-                index0 = int(done[0])
-                index1 = int(done[2])
                 print(NumiCalculator.s0(index0, index1))
             elif done[1] == "*" or done[1] == "times" or done[1] == "Times" or done[1] == "TIMES":
                 """calls the multiplication functio only
                 """
-                index0 = int(done[0])
-                index1 = int(done[2])
                 print(NumiCalculator.m0(index0, index1))
             elif done[1] == "/" or done[1] == "Over" or done[1] == "over" or done[1] == "OVER":
                 """calls the division function only
                 """
-                index0 = int(done[0])
-                index1 = int(done[2])
                 try:
                     print(NumiCalculator.d0(index0, index1))
+                
                 except ZeroDivisionError:
                     print("Division by zero is not allowed")
             else:
@@ -48,39 +49,39 @@ class NumiCalculator():
         elif len(done) == 5:
             """this indicates three values calculation
             """
+            index0 = int(done[0])
+            index1 = int(done[2])
+            index2 = int(done[4])
+            
             if done[1] == '+' or done[1] == 'Plus' or done[1] == "PLUS" or done[1] == 'plus' and done[3] == "+" or done[3] == "plus" or done[3] == "PLUS" or done[3] == "Plus":
                 """calls the addition function only
                 """
-                index0 = int(done[0])
-                index1 = int(done[2])
-                index2 = int(done[4])
                 print(NumiCalculator.a1(index0, index1, index2))
+
             elif done[1] == '-' or done[1] == 'Minus' or done[1] == "MINUS" or done[1] == 'minus' and done[3] == "-" or done[3] == "minus" or done[3] == "MINUS" or done[3] == "Minus":
                 """calls the subtraction function only
                 """
-                index0 = int(done[0])
-                index1 = int(done[2])
-                index2 = int(done[4])
                 print(NumiCalculator.s1(index0, index1, index2))
+
             elif done[1] == '*' or done[1] == 'Times' or done[1] == "TIMES" or done[1] == 'times' and done[3] == "*" or done[3] == "times" or done[3] == "TIMES" or done[3] == "Times":
                 """calls the multiplication functio only
                 """
-                index0 = int(done[0])
-                index1 = int(done[2])
-                index2 = int(done[4])
                 print(NumiCalculator.m1(index0, index1, index2))
+            
             elif done[1] == '/' or done[1] == 'Over' or done[1] == "OVER" or done[1] == 'over' and done[3] == "/" or done[3] == "over" or done[3] == "OVER" or done[3] == "Over":
                 """
                 calls the division function only
                 """
-                index0 = int(done[0])
-                index1 = int(done[2])
-                index2 = int(done[4])
-                print(NumiCalculator.d1(index0, index1, index2))
+                try:
+                    print(NumiCalculator.d1(index0, index1, index2))
+                except ZeroDivisionError:
+                    print("Division by zero is not allowed please...")
+            
             else:
                 print("Unidentified Function call...")
+        
         else:
-            print("Sorry!, I am not programmed for this length of expression yet.\nCheck back when we notify you about an upgrade")
+            print("Sorry!, I am not programmed for this length of calculation yet.\nOr try separating your inputs with a space")
 
 NumiCalculator.main_fxn()
 print("\nCode develoepd by Masino")
